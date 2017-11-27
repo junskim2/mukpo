1774239dc055a78af3f617582a467339eff8ecaa
신주용 <kosta@192.168.0.127>
2017-11-27 15:41:10 +0900

    11271540 사용자 mvc 완료

--- src/user/controller/UserController.java
+++ src/user/controller/UserController.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -50,13 +51,9 @@ public class UserController {
 //		mv.addObject("message",msg);
 //		return mv;
 //	}
-	@RequestMapping("/userPmain.do")
-	public void UserPmain() {
-		
-	}
-	@RequestMapping("/userMmain.do")
-	public void UserMmain() {
-		
+	@RequestMapping(value="/{url}.do")
+	public String url(@PathVariable String url) {
+		return "/user/" + url;
 	}
 	
 }
