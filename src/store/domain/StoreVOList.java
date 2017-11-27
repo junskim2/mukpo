bfeb2ba52f1ac22a449e452783254878d3eb58d9
seonghyun506 <seonghyun506@gmail.com>
2017-11-27 19:55:50 +0900

    list 커밋

---
+++ src/store/domain/StoreVOList.java
@@ -0,0 +1,18 @@
+package store.domain;
+
+import java.util.ArrayList;
+
+public class StoreVOList {
+	private ArrayList<StoreVO> storeList;
+
+	public ArrayList<StoreVO> getStoreList() {
+		return storeList;
+	}
+
+	public void setStoreList(ArrayList<StoreVO> storeList) {
+		this.storeList = storeList;
+	}
+	
+	
+
+}
---
+++ src/user/domain/FavoriteVOList.java
@@ -0,0 +1,15 @@
+package user.domain;
+
+import java.util.ArrayList;
+
+public class FavoriteVOList {
+	private ArrayList<FavoriteVO> favoriteList;
+
+	public ArrayList<FavoriteVO> getFavoriteList() {
+		return favoriteList;
+	}
+
+	public void setFavoriteList(ArrayList<FavoriteVO> favoriteList) {
+		this.favoriteList = favoriteList;
+	}
+}
