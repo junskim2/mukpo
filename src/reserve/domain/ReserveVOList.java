1993553d2a1ad4c55d3a1a33118452b890d27553
신주용 <kosta@192.168.0.127>
2017-11-27 19:57:30 +0900

    11271956 ReserveVOList 생성 완료

---
+++ src/reserve/domain/ReserveVOList.java
@@ -0,0 +1,21 @@
+package reserve.domain;
+
+import java.util.ArrayList;
+
+public class ReserveVOList {
+	
+	private ArrayList<ReserveVO> reserveList;
+
+	public ArrayList<ReserveVO> getReserveList() {
+		return reserveList;
+	}
+
+	public void setReserveList(ArrayList<ReserveVO> reserveList) {
+		this.reserveList = reserveList;
+	}
+
+
+	
+
+
+}
