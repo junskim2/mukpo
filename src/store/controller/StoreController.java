0d08b1eaa8b4660aea3e9af4af5d0ac07e9b21e9
ㅎㅎ <kosta@192.168.0.124>
2017-11-28 19:00:26 +0900

    11281900 컨트롤러 url 화면띄우기

--- src/store/controller/StoreController.java
+++ src/store/controller/StoreController.java
@@ -1,23 +1,16 @@
 package store.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/store")
 public class StoreController {
 
-	@RequestMapping("/storeBossLogin.do")
-	public void storeBossLogin() {
-		
-	}
-	
-	@RequestMapping("/storeMain.do")
-	public void storeMain() {
-		
-	}
-	@RequestMapping("/storeInput.do")
-	public void storeInput() {
-		
-	}
+	// 화면 띄우기
+		@RequestMapping(value="/{url}.do")
+		public String url(@PathVariable String url) {
+			return "/store/" + url;
+		}
 }
