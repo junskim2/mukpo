c833c24ccf830a4f3f82010fb1b38242adc010eb
ㅎㅎ <ell>
2017-11-30 09:55:12 +0900

    11300954 오타수정 파일정리 사진추가

--- src/user/dao/UserDAOImpl.java
+++ src/user/dao/UserDAOImpl.java
@@ -17,80 +17,80 @@ public class UserDAOImpl implements UserDAO{
 	   @Autowired
 	   private SqlSessionTemplate userss;
 
-	public int insertMemberJoin(UserVO vo) { //사용자 회원가입
+	   public int insertMemberJoin(UserVO vo) { //사용자 회원가입
 
-		return 0; 
-	}
+		   return 0; 
+	   }
 
-	@Override
-	public UserVO selectMemberLogin(UserVO vo) { // 사용자 로그인
-		UserVO userVO = null;
-		
-		try {
-			userVO = userss.selectOne("user.userLogin", vo);
-		}catch(Exception e) {
-			System.out.println("UserDAOImpl / idLogin 실패"+ e.getMessage());
+		@Override
+		public UserVO selectMemberLogin(UserVO vo) { // 사용자 로그인
+			UserVO userVO = null;
+			
+			try {
+				userVO = userss.selectOne("user.userLogin", vo);
+			}catch(Exception e) {
+				System.out.println("UserDAOImpl / idLogin 실패"+ e.getMessage());
+			}
+			return userVO;
 		}
-		return userVO;
-	}
-
-	@Override
-	public UserVO updateMemberModify(UserVO vo) { //마이페이지 - 사용자 회원정보수정
-		UserVO userVO = null; 
-
-		return userVO;
-	}
-
-	@Override
-	public UserVO updateMemberDelete(UserVO vo) { //마이페이지 - 회원탈퇴
-		UserVO userVO = null;
-
-		return userVO;
-	}
-
 	
-
-
-	@Override
-	public FavoriteVO selectFS(FavoriteVO vo) { //마이페이지 - 즐겨찾기 상점 목록
-		FavoriteVO favoriteVO = null;
+		@Override
+		public UserVO updateMemberModify(UserVO vo) { //마이페이지 - 사용자 회원정보수정
+			UserVO userVO = null; 
+	
+			return userVO;
+		}
+	
+		@Override
+		public UserVO updateMemberDelete(UserVO vo) { //마이페이지 - 회원탈퇴
+			UserVO userVO = null;
+	
+			return userVO;
+		}
+	
 		
-		return favoriteVO;
-	}
-
-	@Override
-	public ReviewVO selectReview(ReviewVO vo) { //마이페이지 - 내가 쓴 리뷰
-		ReviewVO reviewVO = null;
+	
+	
+		@Override
+		public FavoriteVO selectFS(FavoriteVO vo) { //마이페이지 - 즐겨찾기 상점 목록
+			FavoriteVO favoriteVO = null;
+			
+			return favoriteVO;
+		}
+	
+		@Override
+		public ReviewVO selectReview(ReviewVO vo) { //마이페이지 - 내가 쓴 리뷰
+			ReviewVO reviewVO = null;
+			
+			return reviewVO;
+		}
 		
-		return reviewVO;
-	}
+	//	1130 아름 포인트관련 내용 추가
+		@Override
+		public List<PointVO> selectPoint(PointVO vo) { // 마이페이지 - 포인트 현황
+			return userss.selectList("user.selectPoint", vo);
+		}
 	
-//	1130 아름 포인트관련 내용 추가
-	@Override
-	public List<PointVO> selectPoint(PointVO vo) { // 마이페이지 - 포인트 현황
-		return userss.selectList("user.selectPoint", vo);
-	}
-
-	@Override
-	public int insertPoint(PointVO vo) { // 마이페이지 - 포인트 적립
-
-		return userss.insert("user.insertPoint", vo);
-	}
+		@Override
+		public int insertPoint(PointVO vo) { // 마이페이지 - 포인트 적립
 	
-	@Override
-	public List<PointVO> selectPointDate(PointVO vo) {	//마이페이지 - 먹포인트 현황 (날짜검색)
-		return userss.selectList("user.selectPointDate", vo);
-	}
-
-	@Override
-	public PointVO selectPointCharge(PointVO pointVO) {	// 마이페이지 - 현재포인트
-		return userss.selectOne("user.selectPointCharge", pointVO);
-	}
-
-	@Override
-	public UserVO selectMemberName(UserVO userVO) {	// 회원명 검색
-		return userss.selectOne("user.selectMemberName", userVO);
-	}
+			return userss.insert("user.insertPoint", vo);
+		}
+		
+		@Override
+		public List<PointVO> selectPointDate(PointVO vo) {	//마이페이지 - 먹포인트 현황 (날짜검색)
+			return userss.selectList("user.selectPointDate", vo);
+		}
+	
+		@Override
+		public PointVO selectPointCharge(PointVO pointVO) {	// 마이페이지 - 현재포인트
+			return userss.selectOne("user.selectPointCharge", pointVO);
+		}
+	
+		@Override
+		public UserVO selectMemberName(UserVO userVO) {	// 회원명 검색
+			return userss.selectOne("user.selectMemberName", userVO);
+		}
 	
 //	아름 추가 끝
 
